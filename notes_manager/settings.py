ded275a6033d8275a498bf6c48868a190b519b0e
sadiq-hatake <ameensadiq597@gmail.com>
2025-07-17 23:41:27 +0530

    Initial comm

--- notes_manager/settings.py
+++ notes_manager/settings.py
@@ -142,5 +142,6 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
